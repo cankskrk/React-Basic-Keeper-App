9172ffb152e1eebf69b7516efe1a8fd111ed8fe7
cankskrk <ardacankisakurek@gmail.com>
2023-01-25 22:53:05 +0300

    Update The Style with Material UI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
 import cssHeader from "./cssModules/header.module.css";
 
+import HighlightIcon from "@mui/icons-material/Highlight";
+
 function Header() {
   return (
     <header>
-      <h1 className={cssHeader.title}>Keeper</h1>
+      <h1 className={cssHeader.title}>
+        <HighlightIcon fontSize="large" />
+        Keeper
+      </h1>
     </header>
   );
 }
